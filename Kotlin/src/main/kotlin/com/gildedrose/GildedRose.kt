f460f35b071bcae4e0e687a4d54eabac0a12949c
Mykola Gurov <ngurov@gmail.com>
2023-03-04 13:52:38 +0100

    legacy run!

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,7 +5,8 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
             val item = items[i]
-            updateDailyQuantity(item)
+            //updateDailyQuantity(item)
+            updateDailyQuantityLegacy(item)
         }
     }
 
@@ -14,6 +15,57 @@ class GildedRose(var items: List<Item>) {
         if (item.name == Sulfuras) {
             return // Sulfuras is a legendary item. TODO: enforce 80!
         }
+
+        if (item.name != AgedBrie && item.name != BackstagePasses) {
+            if (item.quality > 0) {
+                if (item.name != Sulfuras) {
+                    item.quality = item.quality - 1
+                }
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+
+                if (item.name == BackstagePasses) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1
+                        }
+                    }
+                }
+            }
+        }
+
+        if (item.name != Sulfuras) {
+            item.sellIn = item.sellIn - 1
+        }
+
+        if (item.sellIn < 0) {
+            if (item.name != AgedBrie) {
+                if (item.name != BackstagePasses) {
+                    if (item.quality > 0) {
+                        if (item.name != Sulfuras) {
+                            item.quality = item.quality - 1
+                        }
+                    }
+                } else {
+                    item.quality = item.quality - item.quality
+                }
+            } else {
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1
+                }
+            }
+        }
+    }
+
+    private fun updateDailyQuantityLegacy(item: Item) {
 
         if (item.name != AgedBrie && item.name != BackstagePasses) {
             if (item.quality > 0) {

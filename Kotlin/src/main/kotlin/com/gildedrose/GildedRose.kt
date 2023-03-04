1463a34496bee759bcb22b578522e3186a9affa0
Mykola Gurov <ngurov@gmail.com>
2023-03-04 14:21:26 +0100

    all good apparently

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,6 +1,6 @@
 package com.gildedrose
 
-typealias SellInDelta = (sellIn: Int) -> Int
+typealias SellInDelta = (sellIn: Int, quality: Int) -> Int
 
 class GildedRose(var items: List<Item>) {
 
@@ -20,52 +20,24 @@ class GildedRose(var items: List<Item>) {
 
         item.sellIn = item.sellIn - 1
 
-        val sellInDelta: SellInDelta = TODO()
-
-        item.quality += (item.quality + sellInDelta(item.sellIn)).coerceAtLeast(0).coerceAtMost(50)
-        if (true) {
-            return
-        }
-
-        if (item.name == AgedBrie || item.name == BackstagePasses) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-
-                if (item.name == BackstagePasses) {
-                    if (item.sellIn < 10) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-
-                    if (item.sellIn < 5) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                }
-            }
-        } else {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1
-            }
-        }
-
-        if (item.sellIn < 0) {
-            if (item.name == AgedBrie) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
-                }
-            } else {
-                if (item.name == BackstagePasses) {
-                    item.quality = 0
+        val sellInDelta: SellInDelta = when {
+            item.name == BackstagePasses -> { sellIn, quality ->
+                if (sellIn < 0) {
+                    -quality
+                } else if (sellIn < 5) {
+                    3
+                } else if (sellIn < 10) {
+                    2
                 } else {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1
-                    }
+                    1
                 }
             }
+            item.name == AgedBrie -> { sellIn, _ -> if (sellIn >= 0) 1 else 2 }
+            else -> { sellIn, _ -> if (sellIn >= 0) -1 else -2 } //by default depreciation 1 by default, -2 after deadline
         }
+
+        val delta = sellInDelta(item.sellIn, item.quality)
+        item.quality = (item.quality + delta).coerceAtLeast(0).coerceAtMost(50)
     }
 
     private fun updateDailyQuantityLegacy(item: Item) {
